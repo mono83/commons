540f402fddd11fcf94df6373580fbd661ab6d75f
Gotterdemarung <gotterdemarung@gmail.com>
2021-02-09 23:00:31 +0200

    :sparkles: Base implementation of interface

--- src/main/java/com/github/mono83/commons/reference/LongIdReference.java
+++ src/main/java/com/github/mono83/commons/reference/LongIdReference.java
@@ -5,4 +5,30 @@ public interface LongIdReference {
      * @return Identifier of entity
      */
     long getId();
+
+    /**
+     * Base implementation of interface.
+     */
+    class Base implements LongIdReference {
+        private final long id;
+
+        public Base(final long id) {
+            this.id = id;
+        }
+
+        @Override
+        public long getId() {
+            return id;
+        }
+
+        @Override
+        public int hashCode() {
+            return Long.hashCode(id);
+        }
+
+        @Override
+        public String toString() {
+            return "{" + getClass().getSimpleName() + " " + getId() + "}";
+        }
+    }
 }
